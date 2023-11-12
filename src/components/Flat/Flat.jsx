866c78ff91c81013eb3953adac6a912633694939
Rodik12rty <Absalyamov.Radmir@gmail.com>
2023-11-12 18:05:45 +0500

    style for addClient button

--- src/components/Flat/Flat.jsx
+++ src/components/Flat/Flat.jsx
@@ -4,18 +4,25 @@ import { User } from "../User/User";
 
 export const Flat = ({flat, setActiveFlat, users, setClientFormFlatId, getItemUsers}) => {
     return (
-        <div onClick={() => {getItemUsers(flat.id); setActiveFlat(flat)}} >
-            {flat.id}. Квартира -- {flat.name}
-
-            <MyButton onClick={() => {setActiveFlat(flat); setClientFormFlatId(flat.id)}}>addClient</MyButton>
-
-            {users && Array.isArray(users[String(flat.id)]) &&
-                <div style={{marginLeft: '10px'}}>
-                    {users[flat.id].map(user =>
-                        <User key={user.id} user={user} />
-                    )}
+        <div>
+            <div onClick={() => {getItemUsers(flat.id); setActiveFlat(flat)}} style={{display: 'flex', justifyContent: 'space-between'}} >
+                <div>
+                    {flat.id}. Квартира -- {flat.name}
                 </div>
-            }
+
+                <MyButton onClick={() => {setActiveFlat(flat); setClientFormFlatId(flat.id)}}>addClient</MyButton>
+
+                
+            </div>
+            <div>
+                {users && Array.isArray(users[String(flat.id)]) &&
+                    <div style={{marginLeft: '10px'}}>
+                        {users[flat.id].map(user =>
+                            <User key={user.id} user={user} />
+                        )}
+                    </div>
+                }
+            </div>
         </div>
     )
 }
